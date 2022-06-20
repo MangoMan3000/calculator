7981d2b8992d0b3204cc93e7ab66b52ae6c9606e
Steven <stevenmancans@gmail.com>
2022-06-20 22:02:52 +1000

    Refactoring code for readibility
    I have removed ids from number buttons and put separate listeners
    across each class, numbers, operators and cut down the long lines of
    code I had before.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //Operator functions
-
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -16,7 +15,14 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-//Take numbers and operator to perform operator function
+//Function to clear stored variables
+function clear() {
+    currentValue = 0;
+    storedValue = null;
+    currentOperator = null;
+}
+
+//Take numbers and operator to perform operation
 function operate(operator, num1, num2) {
     switch (operator) {
         case "add":
@@ -35,35 +41,74 @@ function operate(operator, num1, num2) {
 
 function displayNumber(number) {
     display.textContent = Number(number);
+};
+
+function appendNumber(number) {
+    currentValue += number.toString();
+    displayNumber(currentValue);
+}
+
+function chooseOperator(operator) {
+    currentOperator = operator;
 }
 
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-let leftOperand = 0;
-let rightOperand = 0;
-let currentOperator = "add";
-displayNumber(rightOperand);
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+let currentValue = '';
+let storedValue = null;
+let currentOperator = null;
+displayNumber(currentValue);
 
-//Listen for button press
-buttons.forEach(button => {
-    button.addEventListener("click", e => {
-        if (!isNaN(button.textContent)) {
-            rightOperand += button.textContent.toString();
-            displayNumber(rightOperand);
-        } else if (button.textContent == "C"){
-            leftOperand = 0;
-            rightOperand = 0;
-            currentOperator = "add";
-            displayNumber(rightOperand);
-        } else if (button.className == "operator") {
-            leftOperand = operate(currentOperator, leftOperand, rightOperand);
-            displayNumber(leftOperand);
-            rightOperand = 0;
-            currentOperator = button.id;
-        } else if (button.id == "evaluate") {
-            leftOperand = operate(currentOperator, leftOperand, rightOperand);
-            rightOperand = 0;
-            displayNumber(leftOperand);
-        }
+numberButtons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        appendNumber(button.textContent);
     });
 });
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        chooseOperator(button.id);
+    });
+});
+
+// //Listen for button press
+// buttons.forEach(button => {
+//     button.addEventListener("click", e => {
+//         if (!isNaN(button.textContent)) {
+//             currentValue+= button.textContent.toString();
+//             displayNumber(currentValue);
+//         } else if (button.textContent == "C"){
+//             clear();
+//             displayNumber(currentValue);
+//         } else if (button.className == "operator") {
+//             if (currentOperator == null && storedValue == null) {
+//                 storedValue = currentValue;
+//                 currentValue = 0;
+//                 currentOperator = button.id;
+//             } else if (currentOperator == null) {
+//                 currentOperator = button.id;
+//             } else {
+//                 storedValue = operate(currentOperator, storedValue, currentValue);
+//                 currentOperator = button.id;
+//                 currentValue = 0;
+//                 displayNumber(storedValue);
+//             }
+//         } else if (button.id == "evaluate") {
+//             if (currentOperator == null && storedValue == null) {
+//                 storedValue = currentValue;
+//                 displayNumber(storedValue);
+//                 currentValue = 0;
+//             } else if (currentOperator == null){
+//                 displayNumber(storedValue);
+//                 currentValue = 0;
+//             } else {
+//                 storedValue = operate(currentOperator, storedValue, currentValue);
+//                 displayNumber(storedValue);
+//                 currentValue = 0;
+//                 currentOperator = null;
+//             };
+//         };
+//     });
+// });
